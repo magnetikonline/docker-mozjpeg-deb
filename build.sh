f22fb562ef322cccaee5272af94be52bece76b9e
Peter Mescalchin <peter@magnetikonline.com>
2015-09-15 17:18:48 +1000

    Removed sudo from bash scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build -t magnetikonline/buildmozjpeg .
+docker build -t magnetikonline/buildmozjpeg .
--- extractdeb.sh
+++ extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-containerID=$(sudo docker run -d magnetikonline/buildmozjpeg)
-sudo docker cp $containerID:/root/build/mozjpeg-3.1/build/mozjpeg_3.1-1_amd64.deb .
+containerID=$(docker run -d magnetikonline/buildmozjpeg)
+docker cp $containerID:/root/build/mozjpeg-3.1/build/mozjpeg_3.1-1_amd64.deb .
 sleep 1
-sudo docker rm $containerID
+docker rm $containerID

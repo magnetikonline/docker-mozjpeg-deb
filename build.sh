214a4665d12439f510ca8bbfc241b70c3ca8e94d
Peter Mescalchin <peter@magnetikonline.com>
2015-12-07 20:43:02 +1100

    Exit on error from bash scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/bash -e
 
 docker build -t magnetikonline/buildmozjpeg .
--- extractdeb.sh
+++ extractdeb.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 containerID=$(docker run -d magnetikonline/buildmozjpeg)
 docker cp $containerID:/root/build/mozjpeg-3.1/build/mozjpeg_3.1-1_amd64.deb .
